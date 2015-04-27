4c50463f74371b82d204e711cb2699429ff67952
lingqingmeng <ling.q.meng@gmail.com>
2015-04-26 19:40:02 -0700

    hell yeah

---
+++ routes/views/archives.js
@@ -0,0 +1,40 @@
+var keystone = require('keystone');
+var _ = require('underscore');
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res),
+		locals = res.locals;
+
+	// Set locals
+	locals.section = 'archives';
+	locals.filters = {
+		post: req.params.post
+	};
+	locals.data = {
+		posts: []
+	};
+	
+	// Load other posts
+	view.on('init', function(next) {
+		
+		var q = keystone.list('Post').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('6');
+		
+		q.exec(function(err, results) {
+			
+			locals.data.posts = results;
+			
+			//dbg
+			_.each(locals.data.posts, function(item){
+				console.log('results title',item.title);
+			})
+			//edbg
+			
+			next(err);
+		});
+		
+	});
+	
+	// Render the view
+	view.render('archives');
+	
+};
